d07faa7437b9978c615abdb74c159793a770f40f
Erwan Culerier <erwan.culerier@telecom-paristech.fr>
2017-11-15 22:54:39 +0100

    lololollololol

---
+++ robot_movement/test_main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <math.h>
+#include "movements.h"
+#include "configuration.h"
+#include "ev3.h"
+#include "ev3_port.h"
+#include "ev3_tacho.h"
+// WIN32 /////////////////////////////////////////
+#ifdef __WIN32__
+
+#include <windows.h>
+
+// UNIX //////////////////////////////////////////
+#else
+
+#include <unistd.h>
+#define Sleep( msec ) usleep(( msec ) * 1000 )
+
+//////////////////////////////////////////////////
+#endif
+
+int main(void){
+  turn_left(180);
+  return 0
+}
