6cec3477282fe93a2dd5b1bfffb046c5468d963a
Nathan Biette <nathan1.biette@free.fr>
2018-01-11 17:28:43 +0100

    Translation updated with scan and updated position , not tested

--- src/tacho.h
+++ src/tacho.h
@@ -14,7 +14,7 @@ void stop_tacho(uint8_t tacho_id);
 void wait_wheels(uint8_t right_wheel, uint8_t left_wheel);
 int  waitncheck_wheels(uint8_t right_wheel, uint8_t left_wheel, uint8_t ultrasonic_id);
 void wait_tacho(uint8_t tacho);
-void translation(uint8_t right_wheel, uint8_t left_wheel, int distance);
+int translation(uint8_t right_wheel, uint8_t left_wheel, int distance, uint8_t ultrasonic_tacho, uint8_t ultrasonic_id);
 void rotation(uint8_t right_wheel, uint8_t left_wheel, int angle);
 void rotation_gyro(uint8_t right_wheel, uint8_t left_wheel, uint8_t gyro_id, int angle);
 
@@ -27,6 +27,6 @@ void carrier_up_position(uint8_t obstacle_carrier);
 
 int single_scan(uint8_t ultrasonic_tacho, uint8_t sonar_id, int angle);
 void scan_distance(uint8_t ultrasonic_tacho, uint8_t sonar_id, int number_of_scan, int min_angle, int max_angle, int * array_of_scan_values);
-
+void update_distance(uint8_t right_wheel, uint8_t left_wheel, uint8_t ultrasonic_id, int position_start, int count_per_rot, int previous_traveled_distance, int previous_distance);
 
 #endif
